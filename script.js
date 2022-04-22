038d23174cf5d54723b0c386b5fd0a6ed59917b1
Benjamin Modica <modica.benjamin@gmail.com>
2022-04-22 19:28:54 +0200

    styled, size slider, clear button

--- script.js
+++ script.js
@@ -2,7 +2,52 @@ const container = document.querySelector('.container');
 const PADSIZE = 500;
 let mouseHold = false;
 
+createGrid(50);
+
+// const sizeBtn = document.querySelector('#sizeBtn');
+// sizeBtn.addEventListener('click', replaceGrid);
+
+const sizeSlider = document.querySelector('#sizeSlider');
+sizeSlider.addEventListener('mouseup', (e) => replaceGrid(100 - e.target.value));
+
+const clearBtn = document.querySelector('#clearBtn');
+clearBtn.addEventListener('click', () => replaceGrid(100 - sizeSlider.value));
+
+//Removes old grid and replaces it with new one.
+function replaceGrid(size) {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => container.removeChild(square));
+    createGrid(size);
+    // console.log(size);
+    addEventListeners();
+}
+
+//Eventlisteners for all squares in sketchpad. Will "paint" square if mouse is held down
+function addEventListeners() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', (e) => {
+        e.preventDefault();
+        if (mouseHold) square.classList.add('painted');
+    }));
+}
+
+//Listens for when user holds down mouse
+container.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    mouseHold = true;
+});
+
+//Listens for when user releases mouse
+container.addEventListener('mouseup', (e) => {
+    e.preventDefault();
+    mouseHold = false;
+});
+
+
+
+//Adds size X size grid with divs. 
 function createGrid(size) {
+    if (size > 100) size = 100;
     let squareSize = PADSIZE / size;
 
     for (let i = 0; i < size * size; i++) {
@@ -12,14 +57,5 @@ function createGrid(size) {
         square.style.height = `${squareSize}px`;
         container.appendChild(square);
     }
+    addEventListeners();
 }
-
-createGrid(16);
-
-container.addEventListener('mousedown', () => mouseHold = true);
-container.addEventListener('mouseup', () => mouseHold = false);
-
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => square.addEventListener('mouseover', () => {
-   if(mouseHold) square.classList.add('painted'); 
-}));
